30c5f7ce433c3aa39262725a1b7ec3460c677562
yarncat <30006414+yaaaarn@users.noreply.github.com>
2026-07-13 08:49:34 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,22 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    forAllSystems (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
         packages.default = pkgs.callPackage ./package.nix { };
+
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             go
